ba611169ae40a36fb4900d080c9713b2a5b377ae
DevMMing <mming@stuy.edu>
2018-12-09 21:33:57 -0500

    cleaned files

--- shmegegge.c
+++ shmegegge.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 int key;
-int size;
-int shmflag;
 int shmid;
 char *data;
 char input[99];
